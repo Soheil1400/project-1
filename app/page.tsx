3feac1240d42c4b5fb2937bbea5a52ebb00140c6
soheil <soheil.saedi1379@gmail.com>
2024-12-17 17:29:52 +0330

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,175 @@
+'use client'
+import {Swiper, SwiperSlide, useSwiper} from 'swiper/react';
+import {MdArrowBackIosNew} from "react-icons/md";
+
+import 'swiper/css';
+import {DATABASE} from "@/src/data/data";
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+const DATA = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const DATA_2 = [1, 2, 3, 4, 5, 6, 7, 8]
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+const SwiperNextButton = () => {
+    const swiper = useSwiper();
+    return (
+        <div className={'absolute z-50 -left-4 rounded-full bg-white p-4 top-36 cursor-pointer shadow-shadows-high border-solid border-2 border-surface-alphaLight'}
+             onClick={() => swiper.slideNext()}>
+            <MdArrowBackIosNew style={{color: '#FF00A6'}}/>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    )
+}
+
+const Categories = () => {
+    return (
+        <div className={'flex w-full flex-wrap max-w-7xl mt-52'}>
+            <div className={'w-full my-8 font-bold text-carbon-light text-start '}>
+                {'دسته بندی ها'}
+            </div>
+            <div className={'flex w-full flex-wrap'}>
+                {DATABASE.cats.map(item => (
+                    <div className={'w-1/2 flex md:w-1/6 mb-8'} key={item.id}>
+                        <div
+                            className={'w-3/4 h-24 rounded-xl relative shadow-shadows-medium border-solid border-white border-4 cursor-pointer'}>
+                            <Image src={item.img} alt={item.title} width={100} height={100}
+                                   style={{width: '100%', height: '100%', borderRadius: '12px'}}/>
+                            <div
+                                className={'flex items-center absolute bottom-0 bg-white rounded-tl-xl rounded-br-xl px-3 py-1 -right-1 text-sm'}>
+                                {item.title}
+                                <MdArrowBackIosNew style={{color: '#FF00A6', marginRight: '8px'}}/>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+const Slider = () => {
+    return (
+        <div className={'flex w-full flex-wrap max-w-7xl my-16'}>
+            <div className={'flex justify-between w-full my-4'}>
+                <div>
+                    {'تازه‌ها در اسنپ‌فود'}
+                </div>
+                <div>
+                    {'مشاهده همه'}
+                </div>
+            </div>
+            <div className={'flex w-full relative'}>
+
+                <Swiper
+                    spaceBetween={50}
+                    slidesPerView={4}
+                    onSlideChange={() => console.log('slide change')}
+                    onSwiper={(swiper) => console.log(swiper)}
+                >
+                    <SwiperNextButton/>
+                    {DATA_2.map(item => (
+                        <SwiperSlide key={item}>
+                            <div className={'w-full flex flex-wrap'}>
+                                <div className={'bg-red-500 w-full flex flex-wrap rounded-xl pb-3 cursor-pointer'}>
+                                    <div className={'w-full bg-amber-600 h-36 rounded-t-xl relative'}>
+                                        <div className={'absolute top-8 bg-white rounded-tl-xl rounded-bl-xl px-4'}>
+                                            label
+                                        </div>
+                                    </div>
+                                    <div className={'flex flex-col w-full justify-center items-center -mt-8 z-40'}>
+                                        <div className={'w-20 h-20 bg-blue-600 rounded-xl border-4 border-white'}>
+
+                                        </div>
+                                        <div className={'mt-4'}>
+                                            title
+                                        </div>
+                                        <div className={'my-1'}>
+                                            tag
+                                        </div>
+                                        <div className={'my-1'}>
+                                            description
+                                        </div>
+                                        <div className={'my-1'}>
+                                            delivery
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+        </div>
+    )
+}
+
+const ContactUs = () => {
+    return (
+        <div className={'flex w-full flex-wrap max-w-7xl my-16'}>
+            <div className={'flex w-full bg-amber-800 rounded-xl p-8 justify-between'}>
+                <div className={'w-1/2 flex-colm md:w-full'}>
+                    <div>
+                        {'صاحب کسب و کار هستید؟'}
+                    </div>
+                    <div>
+                        {'با اسنپ فود کسب و کارتان را آنلاین کنید و فروشتان را افزایش دهید.'}
+                    </div>
+                    <div>
+                        <button className={'bg-blue-900'}>
+                            {'ثبت نام فروشندگان'}
+                        </button>
+                    </div>
+                </div>
+                <div className={'w-1/2 hidden justify-end md:flex'}>
+                    image
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const Header = () => {
+    return (
+        <div className={'w-full flex flex-wrap bg-white p-4 fixed shadow-shadows-small z-50'}>
+            <div className={'w-full flex justify-between'}>
+                <div>
+                    {'logo'}
+                </div>
+                <div>
+                    {'search'}
+                </div>
+                <div className={'flex'}>
+                    <div>
+                        {'profile'}
+                    </div>
+                    <div>
+                        {'history'}
+                    </div>
+                </div>
+            </div>
+            <div className={'flex w-full mt-2'}>
+                {DATABASE.services.map((item, index) => (
+                    <div key={index}
+                         className={'flex flex-col w-24 px-1 py-4 rounded-xl justify-center items-center ml-4 bg-surface-main border-2 border-solid border-surface-overlay cursor-pointer'}>
+                        <div className={'w-12 h-12'}>
+                            <Image src={item.img} alt={item.service} width={100} height={100}
+                                   style={{width: '100%', height: '100%'}}/>
+                        </div>
+                        <div className={'mt-4 text-sm'}>
+                            {item.service}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default function Home() {
+    return (
+        <div className={'flex w-full justify-center flex-wrap'}>
+            <Header/>
+            <Categories/>
+            <Slider/>
+            <ContactUs/>
+        </div>
+    );
 }
